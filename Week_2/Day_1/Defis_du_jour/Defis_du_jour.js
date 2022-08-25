17d05a7c85b6e7568d67623033a04782e9b8e221
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-08-25 12:36:20 +0000

    Fin de l'exercice 1 du defis avec succès

---
+++ Week_2/Day_1/Defis_du_jour/Defis_du_jour.js
@@ -0,0 +1,20 @@
+//Defis du jour
+//Exercice 1:
+//1-suppression de Banana
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruits.splice(0,1);
+console.log(fruits);
+//2-Trie du tableau par ordre alphabétique
+fruits.sort();
+console.log(fruits);
+//3-Ajout de Kiwi à la fin du tableau
+fruits.push("Kiwi");
+console.log(fruits);
+//4-Supprimer Apples du tableau
+fruits.shift();
+//5-Trier par ordre decroissant
+fruits.reverse();
+//exercice 1 achevé avec succès.
+
+
+
