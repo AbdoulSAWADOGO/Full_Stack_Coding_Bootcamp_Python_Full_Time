654355c2066d48da857f2f0815c6bcaba1820e38
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-08-26 14:02:23 +0000

    exercice en cours

---
+++ Week_2/Day_2/Exercice_xp/Exercice_xp_day_2.js
@@ -0,0 +1,18 @@
+//Exercice 1:
+//création de deux variables et affectation de valeurs différentes
+let toto=45;
+let titi=12;
+
+if (toto>titi){
+	console.log(toto + " est plus grand que " + titi);
+}
+else {
+	console.log(toto + " est plus petit que " + titi);
+}
+
+//Exercice 2:
+//Chihuahua
+let newDog = "Chihuahua";
+//affichage du nombre de lettres de newDog
+
+
