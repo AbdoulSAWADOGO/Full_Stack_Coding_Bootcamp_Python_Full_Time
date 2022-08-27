e48618ecd3bc5f7c58d62514cbf69ca02447bb64
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-08-27 19:58:12 +0000

    J'avance très bien et je viens de terminer l'exercice 2

--- Week_2/Day_2/Exercice_xp/Exercice_xp_day_2.js
+++ Week_2/Day_2/Exercice_xp/Exercice_xp_day_2.js
@@ -14,5 +14,21 @@ else {
 //Chihuahua
 let newDog = "Chihuahua";
 //affichage du nombre de lettres de newDog
+let nombre_lettres = newDog.length;
+console.log(nombre_lettres);
+//affichage de la du contenu de la variable newDog en majuscule
+newDog.toLocaleUpperCase();
+//affichage de la variable newDog en minuscule
+newDog.toLocaleLowerCase();
+
+newDog = newDog.indexOf("Chihuahua");
+if ( newDog != -1) {
+	console.log("J'adore les chihuahuas, c'est ma race de chien préférée");
+} else {
+    console.log("Je m'en fous, je préfère les chats");  
+}
+//Exercice 2 terminé avec succès. Je n'ai pas rencontré de difficultés particulières
+
+
 
 
