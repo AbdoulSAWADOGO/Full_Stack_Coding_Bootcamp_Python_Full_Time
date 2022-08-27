8d4eb271f8be39910feb246c9ade4d141105c08c
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-08-27 20:20:49 +0000

    Je commence à bien m'amuser Aaa, Exercice 3 achévé avec succès

--- Week_2/Day_2/Exercice_xp/Exercice_xp_day_2.js
+++ Week_2/Day_2/Exercice_xp/Exercice_xp_day_2.js
@@ -29,6 +29,21 @@ if ( newDog != -1) {
 }
 //Exercice 2 terminé avec succès. Je n'ai pas rencontré de difficultés particulières
 
+//Exercice 3
+//récupération du nombre par l'utilisateur
+let nombre = prompt("Svp entrer un nombre!!!");
+//vérification de la parité du nombre
+if (nombre % 2 ==0)
+{
+	console.log(nombre, "est un nombre pair");
+}
+else
+{
+	console.log(nombre, "est un nombre impair");
+}
+//Pas de difficultés rencontrées et achevé avec succès!
+
+
 
 
 
