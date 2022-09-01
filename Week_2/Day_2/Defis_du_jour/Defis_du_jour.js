3686c06f663bbf53b7dd581b498e6af5a9c43b10
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-09-01 11:55:13 +0000

    Solution défis du jour, elle n'est pas opérationnelle car elle ne fait pas exactement ce qu'on a demandé. Je la reverrai pour l'améliorer.

---
+++ Week_2/Day_2/Defis_du_jour/Defis_du_jour.js
@@ -0,0 +1,23 @@
+//Récupération de la chaîne que doit entrer l'utilisateur et affichage
+let sentence = prompt("Exemple://The movie is not that bad, I like it// Alors entrer une phrase contenant les mots not et bad ====>");
+console.log(sentence);
+//Variable de récupération de la première apparition de la sous chaîne not
+let wordNot = sentence.indexOf("not");
+// console.log(wordNot);
+//Variable de récupération de la première apparition de la sous chaine bad
+let wordBad = sentence.indexOf("bad");
+// console.log(wordBad);
+
+//utilisation des conditions et remplacement de certains mots
+if (wordNot < wordBad)
+{
+	// reg = /not|bad/ig;
+	sentence = sentence.replace(/not/ig, "good");
+	sentence = sentence.replace(/bad/ig, "");
+	console.log(sentence);
+}
+else {
+	console.log(sentence);
+}
+
+//La solution ne gère pas correctement ce qu'on a demandé
