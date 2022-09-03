f9c165cde6497ca2e9395d57032772d9af4c768b
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-09-03 04:44:11 +0000

    Première partie terminée avec succès

---
+++ Week_2/day_3/Exercice_XP/Exercice_XP.js
@@ -0,0 +1,30 @@
+//Partie I :
+//Exercice 1 : Examen des tableaux
+//Tableau de travail
+let people = ["Greg", "Mary", "Devon", "James"];
+//Suppression de Greg du tableau
+people.shift();
+//Remplacement de James par jason
+//Il faut d'abord joindre les éléments du tableau
+people = people.join(" ");
+//Ensuite le remplacement
+people = people.replace("James","Jason");
+//Après remplacement, il faut remettre le tableau à l'initial
+people = people.split(" ");
+//Ajout de mon nom à la fin du tableau
+people.push("Abdoul");
+//Code qui console.log l'index de Mary
+let index=people.indexOf("Mary");
+console.log("L'index de Mary est", index);
+//Code pour la copie du tableau
+let toto=people.slice();
+//La copie ne doit pas contenir mon nom ou celui de Mary
+let copie = people.slice(1);
+//Le code qui renvoit l'index de Foo
+let index_Foo = people.indexOf("Foo");
+console.log(index_Foo);
+//Le résultat renvoyé est -1 parce que Foo n'existe pas dans le tableau
+//Une variable last contenant le dernier élément du tableau
+let last = people[people.length-1];
+//Nous pensons que la relation est que la taille du tableau commence à l'index 1 tandis que les éléments du tableau commencent à l'index 0 donc y'aura une différrence de 1 entre le dernier élément du tableau et la taille du tableau.
+//Partie I terminée avec succès.
