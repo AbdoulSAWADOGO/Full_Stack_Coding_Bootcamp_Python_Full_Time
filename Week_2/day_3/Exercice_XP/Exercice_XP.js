e6921e98e7c1d11bbd50e84eb100af503a9349a8
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-09-03 12:11:20 +0000

    fin de la deuxième partie

--- Week_2/day_3/Exercice_XP/Exercice_XP.js
+++ Week_2/day_3/Exercice_XP/Exercice_XP.js
@@ -28,3 +28,20 @@ console.log(index_Foo);
 let last = people[people.length-1];
 //Nous pensons que la relation est que la taille du tableau commence à l'index 1 tandis que les éléments du tableau commencent à l'index 0 donc y'aura une différrence de 1 entre le dernier élément du tableau et la taille du tableau.
 //Partie I terminée avec succès.
+
+
+
+//Partie II : Boucles
+//Parcours du tableau à l'aide d'une boucle
+for ( let i = 0 ; i < people.length ; i ++){
+	console.log(people[i]);
+}
+
+//Parcours du tableau et quitter à la découverte de Jason
+for ( let i in people){
+	console.log(people[i]);
+	if (people[i]=="Jason")
+	{
+		break;
+	}
+}
