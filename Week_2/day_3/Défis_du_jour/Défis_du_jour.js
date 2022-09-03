81a424c0c9d9b76efde5dfbb3d4775a37af34d3c
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-09-03 15:43:32 +0000

    Défis du jour terminé

---
+++ Week_2/day_3/Défis_du_jour/Défis_du_jour.js
@@ -0,0 +1,18 @@
+//Exercice
+//Un programme qui recrée un modèle donné
+//En utilisant une boucle
+
+
+	console.log("*");
+	console.log("**");
+	console.log("***");
+	console.log("****");
+	console.log("*****");
+	console.log("******");
+	let x="*";
+
+for (let i =0; i< 5; i++)
+{
+	console.log(x);
+	x+="*";
+}
