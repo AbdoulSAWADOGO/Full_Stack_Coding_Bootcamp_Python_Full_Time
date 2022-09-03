824b9f3c4e4d128d67dcd52e599406ccd5aaa8c7
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-09-03 13:55:36 +0000

    Exercice XP terminé avec succès

--- Week_2/day_3/Exercice_XP/Exercice_XP.js
+++ Week_2/day_3/Exercice_XP/Exercice_XP.js
@@ -161,3 +161,23 @@ for (let i in details){
 	console.log(i,details[i]);
 } 
 //FIN
+
+
+//Exercice 7 : Groupe secret
+//copie du tableau donné
+let names = ["Jack", "Philip", "Sarah", "Amanda", "Bernard", "Kyle"];
+//recherche du nom de la société
+let nom_société = [];
+//Récupération des lettres
+for( let i in names){
+	nom_société[i] = names[i][0];
+	}
+//trie par ordre alphabétique
+nom_société = nom_société.sort();
+//Jointure des éléments du tableau
+nom_société = nom_société.join("");
+console.log(nom_société);
+//Affichage du nom de la société
+console.log(nom_société);
+
+//Exercice terminé avec succès.
