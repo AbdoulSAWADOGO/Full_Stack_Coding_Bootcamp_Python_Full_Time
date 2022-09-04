d2d37b70caeab61908bc48939f2b2a3cb4eb26e0
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-09-04 18:20:15 +0000

    Exercice 1 terminé

---
+++ Week_2/Day_4/Exercice_XP_OR/Exercice_XP_OR.js
@@ -0,0 +1,16 @@
+//Exercise 1 :
+//Programme vérifiant si une chaîne est vide ou non
+function verif_chaine(chaine) 
+{
+	if (chaine == "")
+	{
+		console.log("True");
+	} 
+	else 
+	{
+		console.log("False");
+	}
+}
+verif_chaine("");
+verif_chaine("abc");
+//Exercice 1 terminé avec succès
