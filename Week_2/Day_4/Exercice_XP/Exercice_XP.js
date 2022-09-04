92273bca3b96a83606ae09a5cb68677570a94e07
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-09-04 14:37:47 +0000

    Fonction à trois arguments terminée avec succès

--- Week_2/Day_4/Exercice_XP/Exercice_XP.js
+++ Week_2/Day_4/Exercice_XP/Exercice_XP.js
@@ -9,3 +9,16 @@ function infoAboutMe()
 //Appel de la fonction
 infoAboutMe();
 //Fin
+//Partie II : Fonctions à trois paramètres
+//Création d'une fonction qui prnds 3 paramètres
+function infoAboutPerson(nom_personne,age_personne,bleu_personne) 
+{
+	console.log("Vous vous appelez " + nom_personne + " .Vous avez " + age_personne + " ans et votre couleur preferée est " + bleu_personne);
+}
+//test de la fonction par appel avec mes exemples
+infoAboutPerson("SAWADOGO",23,"bleu");
+infoAboutPerson("SANKARA",22,"jaune");
+//test de la fonction par appel avec les exemples donnés dans le cours
+infoAboutPerson("David", 45, "blue");
+infoAboutPerson("Josh", 12, "yellow");
+//Fonction à trois paramètres terminée avec succès
