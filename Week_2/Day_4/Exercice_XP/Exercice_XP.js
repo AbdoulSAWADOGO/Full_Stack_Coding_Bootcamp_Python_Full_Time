377cbc59be468a17dda02013113773f934201c92
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-09-04 14:17:51 +0000

    Première partie terminée avec succès

---
+++ Week_2/Day_4/Exercice_XP/Exercice_XP.js
@@ -0,0 +1,11 @@
+//Exercice 1 : Informations
+//Partie I: Fonctions sans paramètres
+//Création de la fonction infoAboutMe()
+
+function infoAboutMe()
+{
+	console.log("Mon nom est SAWADOGO et j'ai 23 ans. Mon loisir est le sport ");
+}
+//Appel de la fonction
+infoAboutMe();
+//Fin
