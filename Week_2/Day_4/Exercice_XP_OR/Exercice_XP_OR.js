31d6e75004a934cc10efa4508e2a42c05531dcb3
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-09-04 18:48:25 +0000

    Exercice 2 terminé

--- Week_2/Day_4/Exercice_XP_OR/Exercice_XP_OR.js
+++ Week_2/Day_4/Exercice_XP_OR/Exercice_XP_OR.js
@@ -14,3 +14,18 @@ function verif_chaine(chaine)
 verif_chaine("");
 verif_chaine("abc");
 //Exercice 1 terminé avec succès
+
+//Exercice 2: Abréviation de nom
+function abbrevName(chaine) 
+{
+	chaine = chaine.split(" ");
+	chaine_abbrev = chaine[0] + " ";
+	for (let i = 1; i < chaine.length; i++)
+	{
+		Lettre_abbrev = chaine[i][0].toUpperCase() + ".";
+		chaine_abbrev = chaine_abbrev + Lettre_abbrev;
+        console.log( "Votre nom en abrégé est ==> ", chaine_abbrev);
+    }
+}
+console.log(abbrevName("Robin Singh"));
+//Exercice 2 terminé
