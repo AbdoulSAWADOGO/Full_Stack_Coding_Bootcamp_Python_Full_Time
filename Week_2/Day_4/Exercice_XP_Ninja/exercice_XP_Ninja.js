0eba545049718b27de54d12b0b644a862f93fc9c
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-09-05 00:14:51 +0000

    Exercice XP Ninja terminé

--- Week_2/Day_4/Exercice_XP_Ninja/exercice_XP_Ninja.js
+++ Week_2/Day_4/Exercice_XP_Ninja/exercice_XP_Ninja.js
@@ -48,4 +48,53 @@ for ( let i = 0; i < chaine.length; i++)
 }
 Majuscule("abcdef");
 
+//Exercice 3 : Est ce un Palindrome
+//Fonction qui vérifie si c'est un palindrome ou pas
 
+
+//Exercice 4 : Plus grand nombre
+function biggestNumberInArray(nombre
+	) {
+	let plus_grand = 0;
+		for (i of nombre)
+		{
+			if (i > plus_grand)
+				plus_grand = i;
+		}
+		console.log(plus_grand);
+			}
+biggestNumberInArray([-1,0,3,100, 99, 2, 99]);
+biggestNumberInArray(['a', 3, 4, 2]);
+biggestNumberInArray([]);
+//Exercice 4 terminé avec succès
+
+//Exercice 5 : Elements uniques
+//Fonction renvoyant uniquement les éléments uniques
+
+function elements_uniques(list) 
+{
+	let nouvelle_liste = [];
+	for (i of list)
+	{
+		let num = 0;
+		for (x of nouvelle_liste)
+		{
+			if (nouvelle_liste == [])
+			{
+				nouvelle_liste[0]=i;
+			}
+			else if (x == i)
+			{
+				num = 1;
+			}
+		if (num==0)
+		{
+			nouvelle_liste[nouvelle_liste.length]=i;
+		}
+		}
+	}
+	console.log(nouvelle_liste);
+}
+elements_uniques([1,2,2,2,2,,3,3,3,3,4,4,5]);
+
+//Exercice 5 terminé avec succès
