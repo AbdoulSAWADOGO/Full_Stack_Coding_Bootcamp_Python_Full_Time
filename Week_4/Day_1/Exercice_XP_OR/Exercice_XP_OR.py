b4b65c62938015be2cf36e18edeeb38a113eea58
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-09-06 16:39:25 +0000

    Exercice XP OR terminé avec succès

---
+++ Week_4/Day_1/Exercice_XP_OR/Exercice_XP_OR.py
@@ -0,0 +1,15 @@
+#Exercice 1 : Hello world-j’aime python
+print("Hello world\n"*4,"I love python\n"*4)
+
+#Exercice 2 : Quelle est la saison
+Mois=int(input("entrer un mois de l’année==>"))
+#afficher la saison du mois
+if (Mois>=3 and Mois<=5) :
+	print(" printemps")
+elif (Mois <=6 and Mois >=8) :
+	print("été")
+elif (Mois <=9 and Mois >=11) :
+	print(" automne ")
+elif (Mois>=12 and Mois<=2) :
+	print("hiver")
+
