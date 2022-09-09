457fe44acf093120cb5a6f6bc9aaab1c9d6ed481
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-09-09 12:27:09 +0000

    Exercice 1 terminé

---
+++ Week_4/Day_2/Exercice_XP_OR/Exercice_XP_OR.py
@@ -0,0 +1,10 @@
+# #Exercice 1 : Concaténer des listes
+# #Code de concaténation de deux listes sans utiliser le signe +
+# from operator import indexOf
+
+
+# liste1 = [1,2,3,4,5]
+# liste2 = [6,7,8,9,10]
+# liste1.extend(liste2)
+# print(liste1)
+
