d99488ed6faf4a48ee7bf3a3e37a5e86f46e89c0
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-09-11 14:37:45 +0000

    Défis du jour OR terminé ainsi que le bonus

--- Week_4/Day_2/Défis_du_jour_OR/Défis_du_jour_OR.py
+++ Week_4/Day_2/Défis_du_jour_OR/Défis_du_jour_OR.py
@@ -7,16 +7,7 @@ i=Naissance[-1]
 annee=Naissance[6:10]
 annee=int(annee)
 print(annee)
-# for i in range(0,int(i)):
-#     t +="i"
-# print("    __",t,"__")
-# print("   |:H:a:p:p:y:|")
-# print(" __|___________|__")
-# print("|^^^^^^^^^^^^^^^^^^|")
-# print("| :B:i:r:t:h:d:a:y:|")
-# print("|                  |")
-# print("~~~~~~~~~~~~~~~~~~~~")
-if(annee%4==0 and annee%100!= 0 or annee%400==0):
+if(annee%4==0 and annee%100!= 0 or annee%400==0):#Bonus
     for x in range(2):
         for i in range(0,int(i)):
             t +="i"
