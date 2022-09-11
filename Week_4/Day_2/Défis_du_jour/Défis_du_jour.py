151a8a898b6ebc564fb5f5c4665945f6c3123ebe
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-09-11 12:42:25 +0000

    Défis du jour terminé

--- Week_4/Day_2/Défis_du_jour/Défis_du_jour.py
+++ Week_4/Day_2/Défis_du_jour/Défis_du_jour.py
@@ -1,13 +1,21 @@
-#Défis 1
-#Demande d'un nombre et d'une taille à l'utilisateur
-liste=[]
-nombre=int(input("Svp entrer un nombre==>"))
-taille=int(input("Svp entrer la taille d'un tableau==>"))
-#Créons un programme qui imprime une liste de multiples de nombre jusqu'à ce que la longueur de la liste atteigne length.
-for i in range(1,taille+1):
-    liste.append(nombre*i)
-print(liste)
+# #Défis 1
+# #Demande d'un nombre et d'une taille à l'utilisateur
+# liste=[]
+# nombre=int(input("Svp entrer un nombre==>"))
+# taille=int(input("Svp entrer la taille d'un tableau==>"))
+# #Créons un programme qui imprime une liste de multiples de nombre jusqu'à ce que la longueur de la liste atteigne length.
+# for i in range(1,taille+1):
+#     liste.append(nombre*i)
+# print(liste)
 
+#Défis 2
+#Demande d'une chaine à l'utilisateur
+new_chaine=""
+chaine=input("entrer une chaine de lettres Svp==>")
+for i in chaine:
+    if new_chaine == '' or i != new_chaine[len(new_chaine)-1]:
+        new_chaine += i
+print(new_chaine)
 
 
 
