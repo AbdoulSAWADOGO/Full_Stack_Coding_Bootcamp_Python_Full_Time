93687d7cce16f4be5f609f379feacdbf9f3e314a
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-09-11 14:35:28 +0000

    Défis du jour OR terminé ainsi que le bonus

---
+++ Week_4/Day_2/Défis_du_jour_OR/Défis_du_jour_OR.py
@@ -0,0 +1,51 @@
+#Demande de la date de naissance de l'utilisateur au format day/mois/année
+Naissance=input("Quelle est votre date d'anniverssaire?\nDonner le jour/le mois/l'année:exemple:05/10/1999==>")
+print(Naissance)
+t=""
+Naissance.split(',')
+i=Naissance[-1]
+annee=Naissance[6:10]
+annee=int(annee)
+print(annee)
+# for i in range(0,int(i)):
+#     t +="i"
+# print("    __",t,"__")
+# print("   |:H:a:p:p:y:|")
+# print(" __|___________|__")
+# print("|^^^^^^^^^^^^^^^^^^|")
+# print("| :B:i:r:t:h:d:a:y:|")
+# print("|                  |")
+# print("~~~~~~~~~~~~~~~~~~~~")
+if(annee%4==0 and annee%100!= 0 or annee%400==0):
+    for x in range(2):
+        for i in range(0,int(i)):
+            t +="i"
+        print("    __",t,"__")
+        print("   |:H:a:p:p:y:|")
+        print(" __|___________|__")
+        print("|^^^^^^^^^^^^^^^^^^|")
+        print("| :B:i:r:t:h:d:a:y:|")
+        print("|                  |")
+        print("~~~~~~~~~~~~~~~~~~~~")
+else:
+    for i in range(0,int(i)):
+        t +="i"
+    print("    __",t,"__")
+    print("   |:H:a:p:p:y:|")
+    print(" __|___________|__")
+    print("|^^^^^^^^^^^^^^^^^^|")
+    print("| :B:i:r:t:h:d:a:y:|")
+    print("|                  |")
+    print("~~~~~~~~~~~~~~~~~~~~")
+
+
+
+
+
+
+
+
+
+
+
+
