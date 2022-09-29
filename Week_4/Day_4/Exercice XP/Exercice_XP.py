d2bb9f94ba7a82c47e23f11ce028d0bb5d6f7258
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-09-29 16:11:05 +0000

    Exercice 4 terminé

--- Week_4/Day_4/Exercice XP/Exercice_XP.py
+++ Week_4/Day_4/Exercice XP/Exercice_XP.py
@@ -15,7 +15,26 @@
 
 #Exercice 3 Un peu de Géographie
 #Écriture d'une fonction appelée describe_city()qui accepte le nom d'une ville et son pays comme paramètres.
-def describe_city(ville_name,country_name="Israèl"):
-    print(f"{ville_name} is in {country_name}")
-describe_city("Ouagadougou","Burkina faso")
-describe_city("Ouagadougou")
+# def describe_city(ville_name,country_name="Israèl"):
+#     print(f"{ville_name} is in {country_name}")
+# describe_city("Ouagadougou","Burkina faso")
+# #test du paramétrage du pays par défaut
+# describe_city("Ouagadougou")
+# #Exercice 3 terminé
+
+
+#Exercice 4 : Aléatoire
+#Création d'une fonction qui accepte un nombre entre 1 et 100 et génère un autre nombre aléatoirement entre 1 et 100.
+import random
+numb = 0
+def Alea_number(numb):
+    # numb=int(input("Svp entrer un nombre entre 1 à 100==>"))
+    Alea = random.randint(1,100)
+    if(Alea == numb):
+        print("Félicitations, vous avez trouvé le secret")
+    else : 
+        print(f"Désolé! vous n'avez pas trouvé le bon nombre. Le bon nombre est {Alea} et pas {numb}")
+while (numb < 1 or numb > 100):
+    print("Vous devez entrer un nombre entre 1 et 100")
+    numb=int(input("Svp entrer un nombre entre 1 à 100==>"))
+Alea_number(numb)
