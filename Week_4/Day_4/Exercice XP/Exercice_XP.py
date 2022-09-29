5208d3d9d446a62f0c162bcb0b997461b417f5b1
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-09-29 15:47:43 +0000

    Exercice 3 terminé

--- Week_4/Day_4/Exercice XP/Exercice_XP.py
+++ Week_4/Day_4/Exercice XP/Exercice_XP.py
@@ -7,8 +7,15 @@
 
 #Exercice 2 : Le livre préféré
 #Écriture d'une fonction appelée favorite_book()qui accepte un paramètre appelé title.
-def favorite_book(title):
-    print(f"One of my favorite books is ==>{title}")
-title = input("Quelle est votre livre préféré ?")
-favorite_book(title)
-#Exercice 2 terminé
+# def favorite_book(title):
+#     print(f"One of my favorite books is ==>{title}")
+# title = input("Quelle est votre livre préféré ?")
+# favorite_book(title)
+# #Exercice 2 terminé
+
+#Exercice 3 Un peu de Géographie
+#Écriture d'une fonction appelée describe_city()qui accepte le nom d'une ville et son pays comme paramètres.
+def describe_city(ville_name,country_name="Israèl"):
+    print(f"{ville_name} is in {country_name}")
+describe_city("Ouagadougou","Burkina faso")
+describe_city("Ouagadougou")
