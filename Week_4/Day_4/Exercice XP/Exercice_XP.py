76871012a64f5e4e1885c6cba217c88b565d4172
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-09-29 15:38:02 +0000

    Exercice 2 terminé

--- Week_4/Day_4/Exercice XP/Exercice_XP.py
+++ Week_4/Day_4/Exercice XP/Exercice_XP.py
@@ -1,5 +1,14 @@
-#Exercice 1 
-#Ecriture d'une fonction appelée display_message() qui imprime une phrase indiquant à tout le monde ce que nous apprenons ici dans ce cours
-def display_message():
-    print("Nous apprenons dans ce cours les fonctions. Nous abordons principalement la création, modification, suppression de fonctions et aussi la différence entre fonctions et methodes")
-display_message()   
+# #Exercice 1 
+# #Ecriture d'une fonction appelée display_message() qui imprime une phrase indiquant à tout le monde ce que nous apprenons ici dans ce cours
+# def display_message():
+#     print("Nous apprenons dans ce cours les fonctions. Nous abordons principalement la création, modification, suppression de fonctions et aussi la différence entre fonctions et methodes")
+# display_message()  
+# #Exercice 1 terminé 
+
+#Exercice 2 : Le livre préféré
+#Écriture d'une fonction appelée favorite_book()qui accepte un paramètre appelé title.
+def favorite_book(title):
+    print(f"One of my favorite books is ==>{title}")
+title = input("Quelle est votre livre préféré ?")
+favorite_book(title)
+#Exercice 2 terminé
