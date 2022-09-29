a0a44eeb56bd17f3c2a803a3ca73c3bb40dc0d04
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-09-29 15:32:29 +0000

    Exercice 1 terminé

---
+++ Week_4/Day_4/Exercice XP/Exercice_XP.py
@@ -0,0 +1,5 @@
+#Exercice 1 
+#Ecriture d'une fonction appelée display_message() qui imprime une phrase indiquant à tout le monde ce que nous apprenons ici dans ce cours
+def display_message():
+    print("Nous apprenons dans ce cours les fonctions. Nous abordons principalement la création, modification, suppression de fonctions et aussi la différence entre fonctions et methodes")
+display_message()   
