ff30c356860d52559e1a13db986cc22223f1acc0
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-09-29 16:25:30 +0000

    Exercice 5 terminé

--- Week_4/Day_4/Exercice XP/Exercice_XP.py
+++ Week_4/Day_4/Exercice XP/Exercice_XP.py
@@ -25,16 +25,30 @@
 
 #Exercice 4 : Aléatoire
 #Création d'une fonction qui accepte un nombre entre 1 et 100 et génère un autre nombre aléatoirement entre 1 et 100.
-import random
-numb = 0
-def Alea_number(numb):
-    # numb=int(input("Svp entrer un nombre entre 1 à 100==>"))
-    Alea = random.randint(1,100)
-    if(Alea == numb):
-        print("Félicitations, vous avez trouvé le secret")
-    else : 
-        print(f"Désolé! vous n'avez pas trouvé le bon nombre. Le bon nombre est {Alea} et pas {numb}")
-while (numb < 1 or numb > 100):
-    print("Vous devez entrer un nombre entre 1 et 100")
-    numb=int(input("Svp entrer un nombre entre 1 à 100==>"))
-Alea_number(numb)
+# import random
+# numb = 0
+# def Alea_number(numb):
+#     Alea = random.randint(1,100)
+#     if(Alea == numb):
+#         print("Félicitations, vous avez trouvé le secret")
+#     else : 
+#         print(f"Désolé! vous n'avez pas trouvé le bon nombre. Le bon nombre est {Alea} et pas {numb}")
+# while (numb < 1 or numb > 100):
+#     print("Vous devez entrer un nombre entre 1 et 100")
+#     numb=int(input("Svp entrer un nombre entre 1 à 100==>"))
+# Alea_number(numb)
+# #Exercice 4 terminé 
+
+#Exercice 5 : Création de chemises personnalisées
+#Écrivons une fonction appelée make_shirt()qui accepte une taille et le texte d'un message qui doit être imprimé sur la chemise.
+def make_shirt(taille=45,texte_of_message="I love python"):
+    print(f"La taille de la chemise est {taille} et le texte est==>{texte_of_message}")
+make_shirt(32, "M. Malick est super instructeur")
+#Réalisation d'une grande chemise avec le message par défaut
+make_shirt(100)
+#Réalisation d'une moyenne chemise avec le message par défaut
+make_shirt(55)
+#Réalisation d'une chemise de taille quelconque avec un message différent
+make_shirt(48, "M. Malick est super cool")
+
+
