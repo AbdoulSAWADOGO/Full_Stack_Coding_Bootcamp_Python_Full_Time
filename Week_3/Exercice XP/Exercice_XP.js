d3e369c838736142c242bed062064c436ad22560
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-10-01 11:39:44 +0000

    Exercice 2 terminé

--- Week_3/Exercice XP/Exercice_XP.js
+++ Week_3/Exercice XP/Exercice_XP.js
@@ -1,3 +1,4 @@
+//Exercice 1 : Utilisateurs
 //Récupération du div et affichage
 var contenu_div = document.getElementById("container").innerHTML;
 console.log(contenu_div);
@@ -32,5 +33,39 @@ for (const elements of Changement_Nom2)
 	}
 }
 //Exercice 1 terminé
-	
+
+//Exercice 2 : Utilisateurs et Style
+//Ajout du code au fichier html
+//Code ajouté
+//Ajout d'une couleur d'arrière plan "bleu clair" et un peu de rembourrage au fichier <div>
+var Changement2=document.body.firstElementChild;
+	console.log(Changement2);
+	Changement2.style.background="lightblue";
+	Changement2.style.padding= "4%";
+//Suppression du <li> contenant le noeud de texte "John"
+// var Changement3=document.getElementsByTagName("ul");
+// 	console.log(Changement3);
+for (const elements of Changement_Nom2)
+{
+	Changement3=elements.textContent
+	if(Changement3==="John"){
+		console.log(Changement3 + " sera supprimé");
+		Changement_Nom4=elements;
+		Changement_Nom4=Changement_Nom4.outerHTML="";
+		console.log(Changement_Nom4);
+	}
+}
+//Ajout d'une bordure contenant le noeud de texte "Pete"
+var Changement4=document.getElementsByTagName("li");
+console.log(Changement4);
+for (const element of Changement4) {
+	if(element.innerText==="Pete"){
+		element.style.border="solid red 3px"	
+	}
+}
+
+//Modification de la taille de police de tout le corps
+var Changement5=document.body;
+Changement5.style.fontSize="50px";
+//Exercice 2 terminé
 
