0190a68bb34316aab2bfccca80a1a308003ff6ed
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-10-05 03:18:22 +0000

    Exercice 3 terminé

--- Week_5/Day_1/Exercice XP/Exercice_XP.js
+++ Week_5/Day_1/Exercice XP/Exercice_XP.js
@@ -55,3 +55,17 @@ else:
 	print(f"{chien_david.nom} est le plus grand ")
 
 //Exercice 2 terminé
+
+//Exercice 3 Qui est le producteur de la chanson
+//Définition d'une classe appelée Song
+class Song:
+	def __init__(self,lyrics):
+		self.lyrics=lyrics
+//La methode sing_me_a_song et impression
+	def sing_me_a_song(self):
+		for i in self.lyrics:
+			print(i)	
+//création d'un objert , copie de l'objet exemple donné clairement				
+toto_objet= Song(["There’s a lady who's sure","all that glitters is gold", "and she’s buying a stairway to heaven"])
+toto_objet.sing_me_a_song()
+//Exercice 3 terminé
