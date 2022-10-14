908df178aef4d2f8dcfa31a8672b623a8a6258fc
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-10-14 12:15:08 +0000

    Exercice XP terminé

--- Week_5/Day_4/Exercice XP/Exercice_XP.py
+++ Week_5/Day_4/Exercice XP/Exercice_XP.py
@@ -28,3 +28,30 @@ def main():
     if length >= 2 and length <= 20:
         print(get_random_sentence(length))
 main()
+
+# Exercice 2 : Travailler Avec JSON
+#Récupération du bout de code donné par l'exercice
+import json
+sampleJson = """{ 
+   "company":{ 
+      "employee":{ 
+         "name":"emma",
+         "payable":{ 
+            "salary":7000,
+            "bonus":800
+         }
+      }
+   }
+}"""
+#Accès à la clé salaire imbriquée à partir de la chaîne JSON ci-dessus
+sampleJson = json.loads(sampleJson)
+print(sampleJson['company']["employee"]['payable']['salary'])
+#Ajout d'une clé appelée "birth_gate" à la chaine JSON au même niveau que la clé Name
+sampleJson['company']['employee'].update({'birth_date':''})
+print(sampleJson)
+
+#Enregistrement du dictionnaire au format JSON dans un fichier
+import json
+jsonFile = 'fichier.json'
+with open(jsonFile,'w') as jF:
+    json.dump(sampleJson,jF)
