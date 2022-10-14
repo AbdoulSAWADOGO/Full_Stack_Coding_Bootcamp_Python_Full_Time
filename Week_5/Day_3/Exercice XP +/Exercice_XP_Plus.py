f9cf02f50054fa3f7bf1a9addbc4931fec1ea923
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-10-14 09:29:20 +0000

    Exercice 5 terminé

--- Week_5/Day_3/Exercice XP +/Exercice_XP_Plus.py
+++ Week_5/Day_3/Exercice XP +/Exercice_XP_Plus.py
@@ -29,3 +29,16 @@ def actu():
     from datetime import datetime, time
     print(datetime.now())
 
+#Exercice 5 : Temps Restant Jusqu'au 1er Janvier
+#Création d'une fonction qui affiche le temps restant jusqu'au 1er janvier.
+def timeRest():
+    import datetime
+    janv = "01/01/2023"
+    janv = datetime.datetime.strptime(janv, "%d/%m/%Y")
+    rest = janv-datetime.datetime.now()
+    rest1= str(rest)
+    print(f"il reste {rest.days} et {rest1[len(rest1)-14:len(rest1)-7]} heures")
+
+timeRest()
+
+
