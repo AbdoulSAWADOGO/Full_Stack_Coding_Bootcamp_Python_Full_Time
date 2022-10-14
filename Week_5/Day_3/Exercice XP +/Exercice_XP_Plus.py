0b862a3314047d942ac4cf8726ad88f872f0cf77
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-10-14 09:40:49 +0000

    Exercice XP Plus terminé

--- Week_5/Day_3/Exercice XP +/Exercice_XP_Plus.py
+++ Week_5/Day_3/Exercice XP +/Exercice_XP_Plus.py
@@ -81,4 +81,22 @@ def age_sur_jupiter():
 
 age_sur_jupiter()
 
+# Exercice 9 : Module Faker
+#installation du module faker et utilisation dans notre code
+users = []#création d'une liste vide appelée users
+#Fonction ajoutant de nouveaux dictionnaires
+def modFaker(users):
+
+    from faker import Faker
+    faker = Faker()
+    langue = ['fr','en','aa','af']
+    lang = str(faker.sentence(ext_word_list = langue)).split(" ")
+    users.append({'nom': faker.name(),'adresse': faker.address(), 'code_langue': lang[0]})
+
+modFaker(users)
+modFaker(users)
+modFaker(users)
+modFaker(users)
+print(users)
+
 
