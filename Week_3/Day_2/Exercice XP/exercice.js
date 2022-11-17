c10da9ba3faa0860343bf2cd794305c29dbd2290
Abdoul Kader SAWADOGO <110843605+AbdoulSAWADOGO@users.noreply.github.com>
2022-11-17 20:54:42 +0000

    Exercice 3 terminé

--- Week_3/Day_2/Exercice XP/exercice.js
+++ Week_3/Day_2/Exercice XP/exercice.js
@@ -38,47 +38,82 @@
 
 //Exercice 2
 
-//Récupération du formulaire et console.log
-console.log(document.body.children[0]);
-//récupération des entrées par leur identifiant et console.log
-console.log(document.getElementById("fname"));
-console.log(document.getElementById("lname"));
-//Récupération des entrées par leur nom d'attribut
-console.log( document.querySelectorAll('[name]'));
-//Lorsque l'utilisateur soumet le formulaire
-document.getElementById("submit").addEventListener("click",function(event){
-event.preventDefault()
-	});
-var changer=document.body.children[0].children[9];
-changer.setAttribute("onclick","addi()")
-console.log(changer);
-function addi(){
-	var change1=document.getElementById("fname").value;
-	var change2=document.getElementById("lname").value;
-	console.log(change1+" and "+change2);
-	if(change1!=="" && change2!==""){
-		console.log("valid input");
-		newli1=document.createElement("li");
-		newTextNode1= document.createTextNode(change1);
-		console.log(newTextNode1);
-		newli1.appendChild(newTextNode1);
-		console.log(newli1);
-		newli2=document.createElement("li");
-		newTextNode2= document.createTextNode(change2);
-		console.log(newTextNode2);
-		newli2.appendChild(newTextNode2);
-		console.log(newli2);
-		change4=changer=document.body.children[1];
-		change4.appendChild(newli1);
-		change4.appendChild(newli2);
-		console.log(change4);
-	}else{
-		console.log(" one of values is invalid try again");
-		alert(" one or both  values are invalid try again");			
-	}
-}
+// //Récupération du formulaire et console.log
+// console.log(document.body.children[0]);
+// //récupération des entrées par leur identifiant et console.log
+// console.log(document.getElementById("fname"));
+// console.log(document.getElementById("lname"));
+// //Récupération des entrées par leur nom d'attribut
+// console.log( document.querySelectorAll('[name]'));
+// //Lorsque l'utilisateur soumet le formulaire
+// document.getElementById("submit").addEventListener("click",function(event){
+// event.preventDefault()
+// 	});
+// var changer=document.body.children[0].children[9];
+// changer.setAttribute("onclick","addi()")
+// console.log(changer);
+// function addi(){
+// 	var change1=document.getElementById("fname").value;
+// 	var change2=document.getElementById("lname").value;
+// 	console.log(change1+" and "+change2);
+// 	if(change1!=="" && change2!==""){
+// 		console.log("valid input");
+// 		newli1=document.createElement("li");
+// 		newTextNode1= document.createTextNode(change1);
+// 		console.log(newTextNode1);
+// 		newli1.appendChild(newTextNode1);
+// 		console.log(newli1);
+// 		newli2=document.createElement("li");
+// 		newTextNode2= document.createTextNode(change2);
+// 		console.log(newTextNode2);
+// 		newli2.appendChild(newTextNode2);
+// 		console.log(newli2);
+// 		change4=changer=document.body.children[1];
+// 		change4.appendChild(newli1);
+// 		change4.appendChild(newli2);
+// 		console.log(change4);
+// 	}else{
+// 		console.log(" one of values is invalid try again");
+// 		alert(" one or both  values are invalid try again");			
+// 	}
+// }
+
+//Exercice 3
+
+//Déclaration d'une variable globale
+		var allBoldItems=[]
+//Créez une fonction appelée getBold_items()qui ne prend aucun paramètre. Cette fonction doit collecter tous les éléments en gras à l'intérieur du paragraphe et les affecter à la allBoldItemsvariable.
+		function getBold_items(){
+			var select=document.getElementsByTagName("strong");
+			console.log(select);
+			i=-1;
+			for (const element of select) {
+				i=i+1;
+				allBoldItems[i]=element;
+			}
+			console.log(allBoldItems);
+		}
+		getBold_items()
+//Créez une fonction appelée highlight()qui change la couleur de tout le texte en gras en bleu
+		function highlight(){
+			for (const elements of allBoldItems) {
+			 	 elements.style.color="blue";
+			}
+		}
+//Créez une fonction appelée return_normal()qui change la couleur de tout le texte en gras en noir.
+		function return_normal(){
+			for (const elements of allBoldItems) {
+			 	 elements.style.color="black";
+			}
+		}
+//Appelez la fonction highlight()sur mouseover (c'est-à-dire lorsque le pointeur de la souris est déplacé sur le paragraphe) et la fonction return_normal()sur mouseout (c'est-à-dire lorsque le pointeur de la souris est déplacé hors du paragraphe).
+		var changer=document.body.children[0]
+		changer.setAttribute("onmouseover","highlight()");
+		changer.setAttribute("onmouseout","return_normal()");
+		console.log(changer);
 
 
+//highlight()
 
 
 
