e953af8c3d11ffc64fff2c80545e828e2bfc52f8
Abdoul Kader SAWADOGO <110843605+AbdoulSAWADOGO@users.noreply.github.com>
2022-11-17 21:15:01 +0000

    exercice 1 terminé

---
+++ Week_3/Day_2/Exercice XP OR/exercice.js
@@ -0,0 +1,18 @@
+//Affichage de la valeur de l'option sélectionnée.
+console.log(document.querySelectorAll('[selected]'));
+var select1=document.querySelectorAll('[selected]')[0].value
+console.log(select1)
+alert(select1)
+//Ajout d'une option supplémentaire au selecttag :
+console.log("-------------------------------------2--------------------------------------------------");
+newoption=document.createElement("option");
+newoption.setAttribute("value","classic");
+newText= document.createTextNode("classic");
+newoption.appendChild(newText);
+console.log(newoption);
+document.getElementById("genres").appendChild(newoption)
+
+
+//L'option nouvellement ajoutée doit être sélectionnée par défaut.
+document.getElementById("genres").children[2].selected=true;
+console.log(document.getElementById("genres").children[2]);
