afea14d2623759be1099f88f0f9331a883e1a76c
Abdoul Kader SAWADOGO <110843605+AbdoulSAWADOGO@users.noreply.github.com>
2022-11-18 18:36:09 +0000

    Exercice XP OR terminé

--- Week_4/Day_3/Exercice_XP OR/Exercice_xp_or.py
+++ Week_4/Day_3/Exercice_XP OR/Exercice_xp_or.py
@@ -59,3 +59,24 @@ if t==0:
 	print(f"we add {name} to the list with birthday date {birth}")
 	birthdays[name]=birth
 	print(birthdays)
+
+
+#Exercice 4
+ item = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+for cle,valeur in item.items():
+    print(cle,"prix-->",valeur)
+somme=0
+items = {
+    "banana": {"price": 4 , "stock":10},
+    "apple": {"price": 2, "stock":5},
+    "orange": {"price": 1.5 , "stock":24},
+    "pear": {"price": 3 , "stock":1}
+    }
+for cle,valeur in items.items():
+    somme+=items[cle]['price']*items[cle]['stock']
+print("Prix total:",somme)
