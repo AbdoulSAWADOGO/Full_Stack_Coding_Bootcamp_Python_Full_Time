b7f5f2161c6f95fdfc5a37809e30bcb93c230841
Abdoul Kader SAWADOGO <110843605+AbdoulSAWADOGO@users.noreply.github.com>
2022-11-18 19:27:08 +0000

    Exercice Ninja terminé

--- Week_4/Day_3/Exercice_XP OR/Exercice_xp_or.py
+++ Week_4/Day_3/Exercice_XP OR/Exercice_xp_or.py
@@ -1,82 +1,31 @@
-#Exercice 1
-#Création de la variable birthdays et sa valeur doit être un dictionnaire
-#Initialisation de la variable avec des anniversaires
-birthday={'Abdoul':'1960/12/24',
-           'Steve':'2009/02/05',
-           'Rachid':'1994/03/29',
-           'Kader':'2003/07/16',
-           'Toto':'2001/06/2001'}
-           #impression message de bienvenue
-print("Welcome, you can see the birthdays of the people in the list below")
-print(birthday)
-t=0
-yname=input("Enter un name:")
-for cle in birthday.keys():
-    if yname==cle:
-        t=1
-        print("The birthday of ",cle)
-        print(birthday[yname])
-if t==0:
-	print("This name is not in our dictionary")
+Importation de reverse et de convert
+from audioop import reverse
+from lib2to3.pytree import convert
 
-
-#Exercice 2
-birthday={'Abdoul':'1960/12/24',
-           'Steve':'2009/02/05',
-           'Rachid':'1994/03/29',
-           'Kader':'2003/07/16',
-           'Toto':'2001/06/2001'}
-           #Impression de tous les noms
-print("Tous les noms ==>",birthdays)
-t=0
-#demande du nom de la personne
-name=input("entre votre nom ==>")
-for cle in birthdays.keys():
-    if name==cle:
-        t=1
-        print("The birthday of ",cle)
-        print(birthdays[name])
-if t==0:
-	#Si la personne n'est pas dans le dictionnaire impression d'un message
- print("Désolé! nous ne connaissons pas votre date d'anniversaire",name)
-
-#Exercice 3
-birthday={'Abdoul':'1960/12/24',
-           'Steve':'2009/02/05',
-           'Rachid':'1994/03/29',
-           'Kader':'2003/07/16',
-           'Toto':'2001/06/2001'}
-name=input("entrer votre nom ==>")
-birth=input("entrer votre date d'anniversaire yyyy/mm/dd ==>")
-t=0
-#Ajout de nouvelles données dans le dictionnaire
-for cle in birthdays.keys():
-    if name==cle:
-        print(f"{name} is found and The corresponding birthday is found:",birthdays[name])
-        t=1
-        break
-if t==0:
-	print(f"we add {name} to the list with birthday date {birth}")
-	birthdays[name]=birth
-	print(birthdays)
-
-
-#Exercice 4
- item = {
-    "banana": 4,
-    "apple": 2,
-    "orange": 1.5,
-    "pear": 3
-}
-for cle,valeur in item.items():
-    print(cle,"prix-->",valeur)
-somme=0
-items = {
-    "banana": {"price": 4 , "stock":10},
-    "apple": {"price": 2, "stock":5},
-    "orange": {"price": 1.5 , "stock":24},
-    "pear": {"price": 3 , "stock":1}
-    }
-for cle,valeur in items.items():
-    somme+=items[cle]['price']*items[cle]['stock']
-print("Prix total:",somme)
+#Copie de la chaine donnée par l'exercice
+chaine="Volkswagen, Toyota, Ford Motor, Honda, Chevrolet    "
+#Conversion en une liste
+liste=chaine.split(',')
+print(liste)
+# Impression des messages
+print("There are {} manufactures/companies in the liste".format(len(liste)))
+print("The list of manufactures in reverse")
+rev=list(reversed(liste))
+print(sorted(rev))
+cpt=0
+n=0
+#Utilisation des boucles de compréhension de liste dans les ordres inverse et descendant
+for i in range(5):
+    if 'o' in liste[i]:
+        cpt+=1
+print(cpt,"manufacture's names have the letter 'o")
+for i in range(5):
+    if 'i' not in liste[i]:
+        n+=1
+print(n,"manufacture's names do not have the letter i")
+liste=["Honda","Volkswagen", "Toyota", "Ford Motor", "Honda", "Chevrolet", "Toyota"]
+conver=set(liste)
+newList=list(conver)
+taille=len(conver)
+print("Set list:",conver)
+print("Il ya ",taille,"element dans la liste:",newList)
