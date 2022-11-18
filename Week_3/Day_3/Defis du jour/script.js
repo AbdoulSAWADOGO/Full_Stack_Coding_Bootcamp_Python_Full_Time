cd9eaf357de954634cdc34a6d46dcd49ceec8e38
Abdoul Kader SAWADOGO <110843605+AbdoulSAWADOGO@users.noreply.github.com>
2022-11-18 16:18:41 +0000

    Défis du jour terminé

--- Week_3/Day_3/Defis du jour/script.js
+++ Week_3/Day_3/Defis du jour/script.js
@@ -1,29 +1,51 @@
-			newinput=document.createElement("input");
-			newinput.setAttribute("placeholder","input only letters please");
-			newinput.setAttribute("type","text");
-			newinput.setAttribute("name","text");
-			newinput.setAttribute("onkeydown","check()");
-			newinput.addEventListener("click",function(event){
-				event.preventDefault()
-			});
-			document.body.appendChild(newinput);
-			
-			function check(){
-				tab=["","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-				console.log(tab)
-				var select=document.body.children[1];
-				var checked=false;
-				console.log(select)
-				console.log(select.value)
-				var i=select.value.substring(select.value.length-1,select.value.length);
-				console.log(i)
-				for (const elements of tab) {
-					if(elements===i || elements.toUpperCase() ===i ){
-				 		 checked=true;
-						console.log(checked)
-					}
-				}
-				if(checked===false){
-					select.value=select.value.substring(0,select.value.length-1);
-				}
-			}
+// Todo list
+let listTasks = [];
+let formulaire,div,contenu,p,check,label;
+let form=document.body.firstElementChild.nextElementSibling;
+let valeur;
+console.log(form);
+function addTask(){
+    document.getElementById("add").addEventListener("click",function(event){
+        valeur=document.getElementById("text").value;
+        if(valeur==""){
+        alert("hello it is  null");
+        }else{
+            alert("it is not null");
+            listTasks=valeur;
+            console.log(listTasks);
+            //atteinte du formulaire
+            formulaire=document.body.firstElementChild.nextElementSibling;
+            //atteinte du div en siblant le prochain element a laide du formualaire
+            div= formulaire.nextElementSibling;
+          //creation dun nouveau contenu avec la nouvelle tache et stocker dans contnu
+            contenu= document.createTextNode(listTasks);
+          //creation dun paragraph p et stocker dans la variable p
+            p=document.createElement("p");
+            //creation du font icone de font aweson
+            var font=document.createElement("i");
+            font.setAttribute("class","fa-sharp fa-solid fa-xmark");
+            //ajout du font dans le paragraph
+            p.appendChild(font);
+            //creation dun ckeckbox par tache
+            check=document.createElement("input");
+            check.type='checkbox';
+            check.id='choix';
+            check.name='choix';
+            label=document.createElement("label");
+            label.htmlFor=contenu.textContent;
+            //ajout du checkbox dans le paragraph 
+            p.appendChild(check);
+            //ajout du label dans le checkbox
+            check.appendChild(label);
+          //ajout du contenu du tableau dans la variable p,le paragraphe
+            p.appendChild(contenu);
+          //ajout du paragraphe dans le div
+            div.appendChild(p);
+
+
+        }
+        event.preventDefault();
+    });
+    
+};
+addTask();
