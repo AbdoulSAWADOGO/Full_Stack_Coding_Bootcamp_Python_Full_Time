002aaa48db706f02e64755ec7055f979c417cb0a
Abdoul Kader SAWADOGO <110843605+AbdoulSAWADOGO@users.noreply.github.com>
2022-11-19 11:15:17 +0000

    Exercice 2 terminé

--- Week_5/Day_1/Exercice XP OR/Exercice_XP_OR.js
+++ Week_5/Day_1/Exercice XP OR/Exercice_XP_OR.js
@@ -20,3 +20,21 @@ géométrie=Circle()
 géométrie.perimeter()
 géométrie.area()
 géométrie.geometric()
+
+
+// Exercice 2 : Classe de liste personnalisée
+
+// Cr&ation de la classe Mylist qui recoit une liste de lettres
+class MyList:
+	def __init__(self,letter):
+		self.letter=letter
+		// Ajout d'une méthode qui renvoie la liste inversée
+	def reverseList(self):
+		print(f'the reversed list= {reversed(self.letter)}')
+		// Ajout d'une méthode qui renvoie la liste triée
+	def sortList(self):
+		print(f'the sorted list= {sorted(self.letter)}')
+		
+list=MyList(['orange','mangoes','tomatoes','potatoes','name','cool','yeh'])
+print(f'La liste inversée==>{list.reverseList()}')
+print(f'La liste triée==>{list.sortList()}')
