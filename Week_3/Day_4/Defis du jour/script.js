87c9e4f58d52d2d32603a397dede0b8f51cf874b
Abdoul Kader SAWADOGO <110843605+AbdoulSAWADOGO@users.noreply.github.com>
2022-11-20 20:53:12 +0000

    Defis du jour terminé

--- Week_3/Day_4/Defis du jour/script.js
+++ Week_3/Day_4/Defis du jour/script.js
@@ -1,51 +1,38 @@
 // Todo list
 let listTasks = [];
 let formulaire,div,contenu,p,check,label;
-let form=document.body.firstElementChild.nextElementSibling;
 let valeur;
-console.log(form);
-function addTask(){
-    document.getElementById("add").addEventListener("click",function(event){
+submit = document.getElementById("add");
+submit.addEventListener("click",function addTask(event){//l'évènement se déclenche lorque le formulaire est soumis
         valeur=document.getElementById("text").value;
         if(valeur==""){
-        alert("hello it is  null");
+        alert("Entrée vide");
         }else{
-            alert("it is not null");
             listTasks=valeur;
-            console.log(listTasks);
-            //atteinte du formulaire
+
+            // utilisation de nextElementSibling
             formulaire=document.body.firstElementChild.nextElementSibling;
-            //atteinte du div en siblant le prochain element a laide du formualaire
-            div= formulaire.nextElementSibling;
-          //creation dun nouveau contenu avec la nouvelle tache et stocker dans contnu
+            div = document.getElementsByClassName("listTasks")[0];
             contenu= document.createTextNode(listTasks);
-          //creation dun paragraph p et stocker dans la variable p
             p=document.createElement("p");
-            //creation du font icone de font aweson
+
+            // ajout de la croix
             var font=document.createElement("i");
-            font.setAttribute("class","fa-sharp fa-solid fa-xmark");
-            //ajout du font dans le paragraph
+            font.setAttribute("class","fa-solid fa-xmark");
             p.appendChild(font);
-            //creation dun ckeckbox par tache
+
+            //ckeckbox
             check=document.createElement("input");
             check.type='checkbox';
             check.id='choix';
             check.name='choix';
             label=document.createElement("label");
             label.htmlFor=contenu.textContent;
-            //ajout du checkbox dans le paragraph 
             p.appendChild(check);
-            //ajout du label dans le checkbox
             check.appendChild(label);
-          //ajout du contenu du tableau dans la variable p,le paragraphe
             p.appendChild(contenu);
-          //ajout du paragraphe dans le div
             div.appendChild(p);
-
-
         }
-        event.preventDefault();
+        document.getElementById("text").value = "";
+        event.preventDefault();//pour que les valeur ne se réinitialise pas 
     });
-    
-};
-addTask();
